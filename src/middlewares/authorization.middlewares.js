d2b276a356cdc7847cd2d26d1aa11756ab343fcd
Chinyere Akanno <adanmachinyere98@gmail.com>
2023-02-21 00:42:34 +0100

    Improved functionality

--- src/middlewares/authorization.middlewares.js
+++ src/middlewares/authorization.middlewares.js
@@ -2,15 +2,16 @@ const authorize = async (req, res, next) => {
   try {
     if (req.user.roles === "admin") {
       return res.send({
+        sucess: true,
         data: `Access granted`,
       });
-    } else {
-      return res.status(403).send({
-        data: `Acess denied, can only view`,
-      });
     }
-
     next();
+
+    return res.status(403).send({
+      success: false,
+      data: `Acess denied, can only view`,
+    });
   } catch (error) {
     res.status(401).send({ success: false, error: "Unauthorized" });
   }
